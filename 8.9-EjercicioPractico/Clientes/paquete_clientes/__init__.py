c3fed39f7d71bc633dda2c80e1ebfcbf6725f747
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-03-23 11:03:35 -0300

    Paquetes de datos

--- 8.9-EjercicioPractico/Clientes/paquete_clientes/__init__.py
+++ 8.9-EjercicioPractico/Clientes/paquete_clientes/__init__.py
@@ -1,4 +1,10 @@
 # __init__.py
+
+#Modulo de cliente
 from .modulo_cliente import Cliente
+
+#Modulo de utilidades
 from .modulo_utilidades import guardar_datos_compras, cargar_datos_compras
-from .modulo_autenticacion import registrar_usuario, login
+
+#Modulo de autenticacion
+from .modulo_autenticacion import registrar_usuario, login, cargar_datos, guardar_datos
